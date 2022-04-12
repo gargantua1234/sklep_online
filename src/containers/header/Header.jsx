fe293c48e7ec5e3baddab303d48b2c6cfbd3dde3
ablech <arek_b1993@poczta.fm>
2022-04-12 20:45:32 +0200

    started creating header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './header.css';
 
 const Header = () => {
-    return <div>Header</div>;
+    return <div className="gpt3__header_section_padding" id="home"></div>;
 };
 
 export default Header;
