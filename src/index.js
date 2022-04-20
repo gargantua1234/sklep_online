8893c60617563a1c8c640b5df0e4cd635d3f2abf
ablech <arek_b1993@poczta.fm>
2022-04-20 10:15:45 +0200

    index.js root content rendering fix

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
